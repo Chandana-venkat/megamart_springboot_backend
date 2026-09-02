73bd250d97425c359daefa625e226687d291ca12
Edupalli Chandana <23kq5a0410ece@gmail.com>
2026-09-02 23:36:32 +0530

    Fix CORS for production frontend

--- src/main/java/com/codegnan/app/javawebapp13082026/megamart/resource/ProductResource.java
+++ src/main/java/com/codegnan/app/javawebapp13082026/megamart/resource/ProductResource.java
@@ -13,7 +13,7 @@ import java.util.List;
 //@CrossOrigin(origins = "http://localhost:5173")
 @CrossOrigin(origins = {
         "http://localhost:5173",
-        "https://megamart-frontend-git-main-chandana-venkats-projects.vercel.app"
+        "https://megamart-frontend-gbhduscuo-chandana-venkats-projects.vercel.app"
 })
 public class ProductResource {
 
