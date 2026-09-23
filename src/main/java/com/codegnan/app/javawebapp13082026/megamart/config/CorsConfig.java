5ef0d6c8303a9ed4d663b8837f3b7a2c4a357a31
Edupalli Chandana <23kq5a0410ece@gmail.com>
2026-09-23 11:14:32 +0530

    Fix CORS configuration

--- src/main/java/com/codegnan/app/javawebapp13082026/megamart/config/CorsConfig.java
+++ src/main/java/com/codegnan/app/javawebapp13082026/megamart/config/CorsConfig.java
@@ -1,60 +1,60 @@
-package com.codegnan.app.javawebapp13082026.megamart.config;
-
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.core.annotation.Order;
-
-import java.util.Arrays;
-
-@Configuration
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173",
-                "https://megamart-frontend-4v0k4pzoj-chandana-venkats-projects.vercel.app",
-                "https://megamart-frontend-six.vercel.app",
-                "https://megamart-frontend-git-main-chandana-venkats-projects.vercel.app"
-        ));
-
-        config.setAllowedMethods(Arrays.asList(
-                "GET",
-                "POST",
-                "PUT",
-                "PATCH",
-                "DELETE",
-                "OPTIONS"
-        ));
-
-        config.setAllowedHeaders(Arrays.asList(
-                "Authorization",
-                "Content-Type",
-                "Accept",
-                "Origin"
-        ));
-
-        config.setExposedHeaders(Arrays.asList(
-                "Authorization"
-        ));
-
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }
-}
+//package com.codegnan.app.javawebapp13082026.megamart.config;
+//
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//import org.springframework.core.annotation.Order;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowedOrigins(Arrays.asList(
+//                "http://localhost:5173",
+//                "https://megamart-frontend-4v0k4pzoj-chandana-venkats-projects.vercel.app",
+//                "https://megamart-frontend-six.vercel.app",
+//                "https://megamart-frontend-git-main-chandana-venkats-projects.vercel.app"
+//        ));
+//
+//        config.setAllowedMethods(Arrays.asList(
+//                "GET",
+//                "POST",
+//                "PUT",
+//                "PATCH",
+//                "DELETE",
+//                "OPTIONS"
+//        ));
+//
+//        config.setAllowedHeaders(Arrays.asList(
+//                "Authorization",
+//                "Content-Type",
+//                "Accept",
+//                "Origin"
+//        ));
+//
+//        config.setExposedHeaders(Arrays.asList(
+//                "Authorization"
+//        ));
+//
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsFilter(source);
+//    }
+//}
--- src/main/java/com/codegnan/app/javawebapp13082026/megamart/config/SecurityConfig.java
+++ src/main/java/com/codegnan/app/javawebapp13082026/megamart/config/SecurityConfig.java
@@ -1,3 +1,158 @@
+//package com.codegnan.app.javawebapp13082026.megamart.config;
+//
+//import com.codegnan.app.javawebapp13082026.megamart.security.JwtAuthenticationFilter;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(
+//            HttpSecurity http,
+//            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+//
+//        http
+//
+//                // Enable CORS
+//                .cors(cors ->
+//                        cors.configurationSource(
+//                                corsConfigurationSource()
+//                        )
+//                )
+//
+//                // Disable CSRF because JWT is used
+//                .csrf(csrf -> csrf.disable())
+//
+//                // Stateless session
+//                .sessionManagement(session ->
+//                        session.sessionCreationPolicy(
+//                                SessionCreationPolicy.STATELESS
+//                        )
+//                )
+//
+//                // Authorization
+//                .authorizeHttpRequests(auth -> auth
+//
+//                        // IMPORTANT: Allow browser CORS preflight
+//                        .requestMatchers(
+//                                HttpMethod.OPTIONS,
+//                                "/**"
+//                        ).permitAll()
+//
+//                        // Authentication
+//                        .requestMatchers(
+//                                "/auth/**"
+//                        ).permitAll()
+//
+//                        // Users
+//                        .requestMatchers(
+//                                "/users",
+//                                "/users/reset-password"
+//                        ).permitAll()
+//
+//                        // Products
+//                        .requestMatchers(
+//                                "/products/**"
+//                        ).permitAll()
+//
+//                        // Cart
+//                        .requestMatchers(
+//                                "/cart/**"
+//                        ).permitAll()
+//
+//                        // Wishlist
+//                        .requestMatchers(
+//                                "/wishlist/**"
+//                        ).permitAll()
+//
+//                        // Other APIs
+//                        .anyRequest().authenticated()
+//                )
+//
+//                // JWT filter
+//                .addFilterBefore(
+//                        jwtAuthenticationFilter,
+//                        UsernamePasswordAuthenticationFilter.class
+//                );
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//
+//        CorsConfiguration configuration =
+//                new CorsConfiguration();
+//
+//        // Frontend URLs
+//        configuration.setAllowedOriginPatterns(
+//                Arrays.asList(
+//                        "http://localhost:5173",
+//                        "https://megamart-frontend-six.vercel.app",
+//                        "https://*.vercel.app"
+//                )
+//        );
+//
+//        // HTTP methods
+//        configuration.setAllowedMethods(
+//                Arrays.asList(
+//                        "GET",
+//                        "POST",
+//                        "PUT",
+//                        "PATCH",
+//                        "DELETE",
+//                        "OPTIONS"
+//                )
+//        );
+//
+//        // Request headers
+//        configuration.setAllowedHeaders(
+//                Arrays.asList("*")
+//        );
+//
+//        // Response headers
+//        configuration.setExposedHeaders(
+//                Arrays.asList(
+//                        "Authorization"
+//                )
+//        );
+//
+//        // Allow credentials
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//
+//        source.registerCorsConfiguration(
+//                "/**",
+//                configuration
+//        );
+//
+//        return source;
+//    }
+//}
+
 package com.codegnan.app.javawebapp13082026.megamart.config;
 
 import com.codegnan.app.javawebapp13082026.megamart.security.JwtAuthenticationFilter;
@@ -33,64 +188,41 @@ public class SecurityConfig {
             JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
 
         http
-
-                // Enable CORS
                 .cors(cors ->
-                        cors.configurationSource(
-                                corsConfigurationSource()
-                        )
+                        cors.configurationSource(corsConfigurationSource())
                 )
 
-                // Disable CSRF because JWT is used
                 .csrf(csrf -> csrf.disable())
 
-                // Stateless session
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS
                         )
                 )
 
-                // Authorization
                 .authorizeHttpRequests(auth -> auth
 
-                        // IMPORTANT: Allow browser CORS preflight
                         .requestMatchers(
                                 HttpMethod.OPTIONS,
                                 "/**"
                         ).permitAll()
 
-                        // Authentication
-                        .requestMatchers(
-                                "/auth/**"
-                        ).permitAll()
+                        .requestMatchers("/auth/**").permitAll()
 
-                        // Users
                         .requestMatchers(
                                 "/users",
                                 "/users/reset-password"
                         ).permitAll()
 
-                        // Products
-                        .requestMatchers(
-                                "/products/**"
-                        ).permitAll()
+                        .requestMatchers("/products/**").permitAll()
 
-                        // Cart
-                        .requestMatchers(
-                                "/cart/**"
-                        ).permitAll()
+                        .requestMatchers("/cart/**").permitAll()
 
-                        // Wishlist
-                        .requestMatchers(
-                                "/wishlist/**"
-                        ).permitAll()
+                        .requestMatchers("/wishlist/**").permitAll()
 
-                        // Other APIs
                         .anyRequest().authenticated()
                 )
 
-                // JWT filter
                 .addFilterBefore(
                         jwtAuthenticationFilter,
                         UsernamePasswordAuthenticationFilter.class
@@ -102,19 +234,15 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration =
-                new CorsConfiguration();
+        CorsConfiguration configuration = new CorsConfiguration();
 
-        // Frontend URLs
         configuration.setAllowedOriginPatterns(
                 Arrays.asList(
                         "http://localhost:5173",
-                        "https://megamart-frontend-six.vercel.app",
                         "https://*.vercel.app"
                 )
         );
 
-        // HTTP methods
         configuration.setAllowedMethods(
                 Arrays.asList(
                         "GET",
@@ -126,28 +254,20 @@ public class SecurityConfig {
                 )
         );
 
-        // Request headers
         configuration.setAllowedHeaders(
                 Arrays.asList("*")
         );
 
-        // Response headers
         configuration.setExposedHeaders(
-                Arrays.asList(
-                        "Authorization"
-                )
+                Arrays.asList("Authorization")
         );
 
-        // Allow credentials
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
 
-        source.registerCorsConfiguration(
-                "/**",
-                configuration
-        );
+        source.registerCorsConfiguration("/**", configuration);
 
         return source;
     }
